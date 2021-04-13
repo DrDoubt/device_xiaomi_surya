25f37df7f1db0ad05b8f57d0def1ef05b4d1b317
DrDoubt <73471150+DrDoubt@users.noreply.github.com>
2021-04-13 17:06:05 +0200

    add rom buildtype and maintainer

--- device.mk
+++ device.mk
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+NEZUKO_BUILD_TYPE := OFFICIAL
+NEZUKO_MAINTAINER := DoubtReaper
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
